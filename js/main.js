8327df5f7c2f3064e512ad02043b728772a7a661
Ali Zarrinkouh <ali78.zarrinkouh@gmail.com>
2021-11-17 00:06:59 +0330

    responsived!

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.display= "block"; 
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.display= "none";
+
+  }
+  function openshop(){
+	  document.getElementById("shopbag").style.display = "block";
+  }
+  function closeshop(){
+	  document.getElementById("shopbag").style.display = "none";
+  }
+  
